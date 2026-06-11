16f1b3b1118b27887d04310c7a5663b1c67ef746
Stefan Eschmann <parhelik@gmail.com>
2026-06-11 23:10:25 +0100

    Added init.el file

---
+++ init.el
@@ -0,0 +1,19 @@
+;; init.el --- My Emacs Config
+
+
+
+;; Cursor settings
+(setq-default cursor-type 'bar)
+
+;; Disable Startup screen
+(setq inhibit-startup-screen t)
+
+;; Enable ido mode
+(ido-mode t)
+
+;; UI Settings
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; init.el ends here
