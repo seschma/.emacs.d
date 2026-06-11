996e1645ab00cba018fcfee5f00af5da0303bfcb
Stefan Eschmann <parhelik@gmail.com>
2026-06-12 00:42:17 +0100

    Updates

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
 ;; init.el --- My Emacs Config
 
+;;
 
+
+;; Set scratch message
+(setq initial-scratch-message "")
 
 ;; Cursor settings
 (setq-default cursor-type 'bar)
@@ -8,8 +12,8 @@
 ;; Disable Startup screen
 (setq inhibit-startup-screen t)
 
-;; Enable ido mode
-(ido-mode t)
+;; Enable fido vertical mode (Built-in)
+(fido-vertical-mode 1)
 
 ;; UI Settings
 (tool-bar-mode -1)
